00c77de1d11bfb99cf29fa13b94c00107bb91831
AlexMa <onlyalex1995@gmail.com>
2020-04-13 05:24:45 +0000

    add more support

---
+++ env.sh
@@ -0,0 +1,12 @@
+export ZSH_THEME=powerlevel10k/powerlevel10k
+export TERM=xterm-256color
+alias vim=/datadrive/alex/home/projects/SetupForMe/nvim.appimage
+alias python3=/usr/bin/python3.7
+alias python=/usr/bin/python3.7
+
+
+export PATH="/datadrive/alex/home/.local/bin:$HOME/lib/x86_64-linux-gnu:$HOME/lib:$HOME/.local/lib:$PATH"
+export LD_LIBRARY_PATH="$HOME/lib/x86_64-linux-gnu:$HOME/lib:$HOME/.local/lib:$LD_LIBRARY_PATH"
+export PYTHONPATH="$HOME/lib/x86_64-linux-gnu:$HOME/lib:$HOME/.local/lib:$PYTHONPATH"
+
+alias zshconfig="vim /datadrive/alex/home/projects/SetupForMe/env.sh"
--- setup.sh
+++ setup.sh
@@ -11,5 +11,23 @@ else
 	make
 	make install
 	echo "[ -f $HOME/bin/zsh ] && exec $HOME/bin/zsh -l" >> ~/.profile
+  cd ..
 fi
 
+# oh-my-zsh install
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+#sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+rm env.sh
+touch env.sh
+env_path=$(realpath env.sh)
+
+# powerlevel10k install
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+echo "ZSH_THEME=powerlevel10k/powerlevel10k" >> env.sh
+
+# neovim install 
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x nvim.appimage
+vim_path=$(realpath nvim.appimage)
+echo "alias vim=${vim_path}" >> env.sh
