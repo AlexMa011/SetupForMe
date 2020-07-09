021504cf6225ddc9eb24bf443e6dfdcc7d0e816a
AlexMa <onlyalex1995@gmail.com>
2020-07-09 05:32:53 +0000

    remove env.sh

--- generate_env.sh
+++ generate_env.sh
@@ -13,9 +13,13 @@ echo 'export TERM=xterm-256color' >> env.sh
 echo >> env.sh
 
 echo '# path setting' >> env.sh
-echo 'export PATH=~/.local/bin:$PATH' >> env.sh
+echo 'export PATH="~/.local/bin:$PATH" ' >> env.sh
 echo >> env.sh
 
+echo '# zsh condig editor' >> env.sh
+echo 'export PATH="~/.local/bin:$PATH" ' >> env.sh
+zshconfig="vim $(realpath ~/.zshrc)"
+echo "alias zshconfig=\"${zshconfig}\" " >> env.sh
 envconfig="vim $(realpath env.sh)"
 echo "alias envconfig=\"${envconfig}\" " >> env.sh
 
