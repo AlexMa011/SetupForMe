0fe252805e0583472876e84e6f3b99c652c3828d
AlexMa <onlyalex1995@gmail.com>
2020-08-14 07:28:16 +0000

    rm personal info

--- setup.sh
+++ setup.sh
@@ -19,10 +19,9 @@ fi
 if [ ! -e ~/.oh-my-zsh/custom/themes/powerlevel10k ]
 then
 	git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-  cat ~/.zshrc | sed 's#ZSH_THEME=.*#ZSH_THEME="powerlevel10k/powerlevel10k"#g' > zsh_config
-  mv zsh_config ~/.zshrc
-  source ~/.zshrc
 fi
+cat ~/.zshrc | sed 's#ZSH_THEME=.*#ZSH_THEME="powerlevel10k/powerlevel10k"#g' > zsh_config
+mv zsh_config ~/.zshrc
 echo 'powerlevel10k installed!'
 
 # neovim install 
